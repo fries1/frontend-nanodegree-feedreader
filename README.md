8e7c17bee5dc32e3ecab21692cc3f176bef067a8
Stefan Fries <stefanefries@googlemail.com>
2015-06-20 17:39:34 +0200

    all done

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+to run: - Retain the given file structure.
+		- Run the application by loading index.html.
+
 # Project Overview
 
 In this project you are given a web-based application that reads RSS feeds. The original developer of this application clearly saw the value in testing, they've already included [Jasmine](http://jasmine.github.io/) and even started writing their first test suite! Unfortunately, they decided to move on to start their own company and we're now left with an application with an incomplete test suite. That's where you come in.
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -14,6 +14,8 @@ $(function() {
     * feeds definitions, the allFeeds variable in our application.
     */
     describe('RSS Feeds', function() {
+        var len = allFeeds.length;
+
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
@@ -23,7 +25,7 @@ $(function() {
          */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
+            expect(allFeeds.len).not.toBe(0);
         });
 
 
@@ -31,22 +33,41 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('each feed has a non-empty URL', function() {
+            for(var i = 0; i < len; i++){
+                expect(allFeeds[i].url).not.toBe('');
+                // should be the same as toBeDefined();
+                expect(allFeeds[i].url).not.toBe(undefined);
+            }
+         });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('each feed has a non-empty name', function() {
+            for(var i = 0; i < len; i++){
+                expect(allFeeds[i].name).not.toBe('');
+                expect(allFeeds[i].name).not.toBe(undefined);
+            }
+         });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         it('is hidden by default', function() {
+            // creates an array that holds all of body's classes
+            var classes = document.body.className.split(/\s+/);
+            // tests if body as a class 'menu-hidden'
+            expect(classes).toContain('menu-hidden');
+         });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
@@ -54,19 +75,78 @@ $(function() {
           * clicked and does it hide when clicked again.
           */
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+          var menuIcon = $('.menu-icon-link');
+          var clicked = false;
+          it('should change visibility when the menu icon is clicked', function(){
+            // trigger an Icon click
+            menuIcon.trigger('click');
+            // now the menu should be visible
+            var classes = document.body.className.split(/\s+/);
+            expect(classes).not.toContain('menu-hidden');
+            // trigger another click
+            menuIcon.trigger('click');
+            // now it should be hidden
+            classes = document.body.className.split(/\s+/);
+            expect(classes).toContain('menu-hidden');
+          });
 
+    });
+
+    /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test wil require
+         * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+        // call the loadFeed function and make sure to wait till it is finsihed
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+        // now test if .feed has child elements, i.e. entries
+        it('should have at least one .entry element within the .feed container', function(done){
+            expect($('.feed').children().length).toBeGreaterThan(0);
+            done();
+        });
+    })
 
+
+    /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+         // to test if the content changes with new further calls to
+         // loadFeed(), the .header-title of two consecutive calls with
+         // different parameters are compared
+        var header1, header2;
+        beforeEach(function(done) {
+            // first call to loadFeed()
+            loadFeed(1, function() {
+                header1 = $('.header-title')[0].textContent;
+                //console.log(header1);
+            });
+
+            // second call to loadFeed after 1s
+            setTimeout(function() {
+                loadFeed(2, function() {
+                    header2 = $('.header-title')[0].textContent;
+                    //console.log(header2);
+                    done();
+                });
+            }, 1000);
+        });
+
+        // after both calls are finished the header-titles are comapred
+        it('should result in the content to change', function(done){
+
+            expect(header1).not.toEqual(header2);
+            done();
+        });
+    })
 }());
